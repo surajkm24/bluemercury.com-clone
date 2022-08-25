59438f6da39f832323b034b45d189f201e58c1d6
surajkm24 <surajkm336796@gmail.com>
2022-08-25 10:50:18 +0530

    new products slider component created

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import { Navbar } from './components/Navbar';
 import {ChakraProvider} from '@chakra-ui/react';
 import { HeaderCarousel } from './components/HeaderCarousel';
+import { NewArrivals } from './components/NewArrivals';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
       <ChakraProvider>
         <Navbar/>
         <HeaderCarousel/>
+        <NewArrivals/>
       </ChakraProvider>
     </div>
   )
