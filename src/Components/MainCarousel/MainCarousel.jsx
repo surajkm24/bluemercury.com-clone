6c3a9b019fab92ff45c18e28db8a54ed964094c0
surajkm24 <surajkm336796@gmail.com>
2023-01-08 22:38:17 +0530

    skeleton loader added to improve ui

--- src/Components/MainCarousel/MainCarousel.jsx
+++ src/Components/MainCarousel/MainCarousel.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useRef, useState } from "react";
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, Skeleton } from '@chakra-ui/react';
 import { SlideOne } from "./SlideOne";
 import { SlideTwo } from "./SlideTwo";
 import { SlideThree } from "./SlideThree";
 
 export const MainCarousel = () => {
     const [slide, setSlide] = useState(1);
+    const [loading, setLoading] = useState(true);
     const slideRef = useRef(null);
     const clickSlide = () => {
         clearInterval(slideRef.current);
@@ -13,6 +14,13 @@ export const MainCarousel = () => {
             setSlide(prev => prev === 1 ? 2 : prev === 2 ? 3 : 1)
         }, 5000)
     }
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 1000)
+    }, []);
+
     useEffect(() => {
         slideRef.current = setInterval(() => {
             setSlide(prev => prev === 1 ? 2 : prev === 2 ? 3 : 1)
@@ -22,19 +30,23 @@ export const MainCarousel = () => {
         }
     }, [])
     return (
-        <Box className='carousel' >
-            <SlideOne slide={slide} />
-            <SlideTwo slide={slide} />
-            <SlideThree slide={slide} />
-            <Flex gap='10px' justify='center' p='15px'>
-                {new Array(3).fill(1).map((ele, i) => (
-                    <Box key={i + 1} border='1px solid black' borderRadius={'50%'}
-                        bg={slide === (i + 1) ? "black" : "white"} h='12px' w='12px' cursor='pointer'
-                        onClick={() => { setSlide((i + 1)), clickSlide() }}>
-                    </Box>
-                ))}
-            </Flex>
-        </Box>)
+        <Skeleton h='30vw' isLoaded={!loading}>
+            <Box className='carousel' >
+                <SlideOne slide={slide} />
+                <SlideTwo slide={slide} />
+                <SlideThree slide={slide} />
+                <Flex gap='10px' justify='center' p='15px'>
+                    {new Array(3).fill(1).map((ele, i) => (
+                        <Box key={i + 1} border='1px solid black' borderRadius={'50%'}
+                            bg={slide === (i + 1) ? "black" : "white"} h='12px' w='12px' cursor='pointer'
+                            onClick={() => { setSlide((i + 1)), clickSlide() }}>
+                        </Box>
+                    ))}
+                </Flex>
+            </Box>
+        </Skeleton>
+    )
+
 }
 
 
